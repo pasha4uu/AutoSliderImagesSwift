b64fe0e52d5fb8bcc229b9b4c964b6c36c25108a
pasha4uu <pasha7864u@gmail.com>
2019-02-06 22:45:35 +0530

    Initial Commit

---
+++ AutoSliderImagesSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoSliderImagesSwift
+//
+//  Created by PASHA on 06/02/19.
+//  Copyright © 2019 Reatchall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ AutoSliderImagesSwiftTests/AutoSliderImagesSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  AutoSliderImagesSwiftTests.swift
+//  AutoSliderImagesSwiftTests
+//
+//  Created by PASHA on 06/02/19.
+//  Copyright © 2019 Reatchall. All rights reserved.
+//
+
+import XCTest
+@testable import AutoSliderImagesSwift
+
+class AutoSliderImagesSwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
